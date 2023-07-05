de43b406440255a4233dd4e221062b4555ddde5b
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:16:27 +0200

    add app

---
+++ MethodOverridingHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverridingHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
